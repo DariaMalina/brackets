4d5754548d0f3f1ed36b20e8aab65c751a46f606
Daria Malina <gluto68@gmail.com>
2021-08-27 12:16:43 +0300

    feat: completed task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brack = bracketsConfig.map(el=> el.join(''));
+    let answer = true;
+    while (str.length && answer) {
+        answer = false;
+        for (let i = 0; i < brack.length; i++) {
+            let basicPosition = str.indexOf(brack[i]);
+            if (basicPosition !== -1) {
+                str = str.slice(0, basicPosition) + str.slice(basicPosition + 2);
+                answer = true;
+            }
+        }
+    }
+    return !str.length;
 }
