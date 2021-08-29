e23234ea39f8f7d3753dacfd850ce486440cc46b
Daria Malina <gluto68@gmail.com>
2021-08-30 02:47:48 +0300

    feat: implementation of another solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openPair = []
+    const closePair = {}
+    for (let a = 0; a < bracketsConfig.length; a++) {
+        openPair.push(bracketsConfig[a][0])
+        closePair[bracketsConfig[a][1]] = bracketsConfig[a][0]
+    }
+
+    let stack = []
+    for (let i = 0; i < str.length; i++) {
+        let currentElem=str[i];
+
+
+        if (openPair.includes(currentElem)) {
+            if (str[i]===closePair[currentElem]&&stack.includes(str[i])){
+                stack.pop()
+            }else {
+                stack.push(currentElem)}
+
+        } else {
+            if (stack.length === 0) {
+                return false
+            }
+
+            let lastElementStack = stack[stack.length - 1]
+            if (closePair[currentElem] === lastElementStack) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0
 }
